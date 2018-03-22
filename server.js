3155ac49e8a69612930c4a429e5658404147952f
Brian R Murphy <BrianRMurphy89@gmail.com>
2018-03-22 11:37:46 -0400

    updated everything for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
@@ -10,6 +10,14 @@ app.use(express.urlencoded({extended:false}));
 const marvelController = require('./controllers/marvel.js');
 app.use('/home', marvelController);
 
+
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/marvel';
+mongoose.connect(mongoURI);
+
+mongoose.connection.once('open', () => {
+  console.log('connected to mongo');
+})
 app.listen(port, () => {
   console.log('Infinity War is coming!');
 })
